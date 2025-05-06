827fe5653918ca3c8f708598adaeeee761446cba
haidarniv <haidarapps12@gmail.com>
2025-05-06 11:50:05 +0700

    fix the widht and checked box

--- docs/script.js
+++ docs/script.js
@@ -8,16 +8,22 @@ document.addEventListener('DOMContentLoaded', () => {
         emptyImage.style.display = taskList.children.length === 0 ? 'block' : 'none';
     }
 
+    // if theres an added task
     const addTask = (event) => {
         event.preventDefault();
         const taskText = taskInput.value.trim();
         if (!taskText) return;
 
+        if(taskText.length > 30) {
+            alert(`bruh, the task is too long brotha. Yours is ${taskText.length}`);
+            return;
+        }
+
         const li = document.createElement('li');
         li.innerHTML = `
             <input type="checkbox" class="checkbox" />
-            <span class="task-text">${taskText}</span>
-            <div class="task-buttons">
+            <span class="task-text break-words max-w-sm w-full">${taskText}</span>
+            <div class="task-buttons space-x-2 flex flex-row">
                 <button class="edit-btn"><i class="fa-solid fa-pen"></i></button>
                 <button class="delete-btn"><i class="fa-solid fa-trash"></i></button>
             </div>
